f6808f1836705d1443f723e8643062d66049a6be
MeenuPrudhvija <125639922+MeenuPrudhvija@users.noreply.github.com>
2024-06-12 12:05:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, jsonify
 from motion_detection import motionDetection
+from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('template/index.html')
+    return render_template('index.html')
 
 @app.route('/start-motion-detection')
 def start_motion_detection():
-    motionDetection()
-    return jsonify(message='Motion detection completed')
+    t = Thread(target=motionDetection)
+    t.start()
+    return jsonify(message='Motion detection started')
 
 if __name__ == "__main__":
     app.run(debug=True)
